ed05a76577adb12fac9abb6a67ff62425fdc465b
CK Nivens <Nivens1084C@ckstudents.org>
2018-02-20 16:27:52 -0800

    Added Elevator code

--- src/commands/elevatorScaleHeight.py
+++
@@ -1,7 +0,0 @@
-import wpilib.command
-import ctre
-
-class elevatorScaleHeight(wpilib.command.Command):
-    def __init__(self):
-        super().__init__("elevatorScaleHeight")
-        #Add finishing stuff that will have the motors and controls for the controller. This will be for button Y!
--- src/commands/elevator_test.py
+++ src/commands/elevator_test.py
@@ -9,14 +9,15 @@ class ElevatorTest(Command):
         self.requires(self.elevator)
 
     def execute(self):
+        '''
         x = self.joystick.getRawAxis(1)
         if abs(x) < 0.1:
             x = 0
         self.elevator.test_drive_x(x)
         '''
-        if self.joystick.getRawButton(7):
+        if self.joystick.getRawButton(1): #A pressed
             self.elevator.test_drive_positive()
-        elif self.joystick.getRawButton(8):
+        elif self.joystick.getRawButton(2): #B pressed
             self.elevator.test_drive_negative()
         else:
-            self.elevator.off()'''
+            self.elevator.off()
